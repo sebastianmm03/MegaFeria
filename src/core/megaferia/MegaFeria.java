7d16bc300b32613d661c673fe6cda4a26209a966
Sebastian Andres Mercado Martinez <asmercado@uninorte.edu.co>
2024-03-08 10:19:58 -0500

    Packages Creados

--- src/core/megaferia/MegaFeria.java
+++ src/core/megaferia/MegaFeria.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package megaferia;
+package core.megaferia;
 
 /**
  *
@@ -11,7 +11,7 @@ package megaferia;
 public class MegaFeria {
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo!!!");
     }
     
 }
