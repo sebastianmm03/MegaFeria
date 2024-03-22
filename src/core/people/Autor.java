b52d2fb9dcac742f30677f771b9242e00f9ac675
Sebastian Andres Mercado Martinez <>
2024-03-22 09:19:33 -0500

    Cambio en Autor

--- src/core/people/Autor.java
+++ src/core/people/Autor.java
@@ -4,10 +4,20 @@
  */
 package core.people;
 
+import core.libro.Libro;
+import java.util.ArrayList;
+
 /**
  *
  * @author asmercado
  */
 public class Autor {
+    
+    private ArrayList<Libro> libros;
+
+    public Autor(String nombre., int cedula) {
+        super(nombre, cedula);
+        this.libros = new ArrayList<>();
+    }
     
 }
